3ba8616d778c29816909c6e4ea1d4227a96bd8d7
apiduru <apiduru@ncsu.edu>
2024-11-07 22:21:08 -0500

    [Implementation] Task test in progress

--- Project2/test/edu/ncsu/csc216/wolf_tracker/model/task/TaskTest.java
+++ Project2/test/edu/ncsu/csc216/wolf_tracker/model/task/TaskTest.java
@@ -2,13 +2,68 @@ package edu.ncsu.csc216.wolf_tracker.model.task;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TaskTest {
+import edu.ncsu.csc216.wolf_tracker.model.log.CategoryLog;
 
+/**
+ * Tests the Task class.
+ * 
+ * @author Anoushka Piduru
+ */
+class TaskTest {
+	/** Task instance for testing. */
+	private Task task;
+	/** CategoryLog instance for testing. */
+	private CategoryLog category;
+
+	/**
+	 * Tests the task title.
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented"); // TODO
+	void testTaskTitle() {
+		task = new Task("Task", 45, "Description");
+		assertEquals("Task", task.getTaskTitle());
+
+		// Null title
+		assertThrows(IllegalArgumentException.class, () -> new Task(null, 45, "Description"));
+	}
+
+	/**
+	 * Tests the task duration.
+	 */
+	@Test
+	void testTaskDuration() {
+		task = new Task("Task", 45, "Description");
+		assertEquals(45, task.getTaskDuration());
+
+		// Null duration
+		assertThrows(IllegalArgumentException.class, () -> new Task("Task", -5, "Description"));
+	}
+
+	/**
+	 * Tests the task description.
+	 */
+	@Test
+	void testTaskDetails() {
+		task = new Task("Task", 45, "Description");
+		assertEquals("Description", task.getTaskDetails());
+
+		// Null details
+		assertThrows(IllegalArgumentException.class, () -> new Task("Task", 45, null));
+	}
+
+	/**
+	 * Tests the add category method.
+	 */
+	@Test
+	void testAddCategory() {
+		task = new Task("Task", 45, "Description");
+		assertEquals("Description", task.getTaskDetails());
+
+		// Null category
+		assertThrows(IllegalArgumentException.class, () -> task.addCategory(null));
 	}
 
 }
